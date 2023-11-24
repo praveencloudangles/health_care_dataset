d598a4bac4b9dfd611213e611f08e0d3fbd80a90
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2023-11-24 11:14:27 +0530

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -2,14 +2,14 @@ from data_loading import data_loading
 
 def data_analysis():
     data = data_loading()
-    print("information----------",data.info())
-    print("describe----------",data.describe())
-    print("column names-----------",data.columns)
-    print("shape---------",data.shape)
-    print("null values--------------",data.isnull().sum())
+    data.info()
+    data.describe()
+    data.columns
+    data.shape
+    data.isnull().sum()
     for col in data.columns:
         print(col, data[col].nunique())
 
-    print("duplicate values---------",data.describe().sum())
+    data.describe().sum()
     return data
 data_analysis()
